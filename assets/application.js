cd3293efd0d0c24358c6a2f978a61e7bf3fd5343
ben <gibumkang@gmail.com>
2020-06-17 06:58:43 -0700

    Add swiper slider and transparent header

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,32 @@
-// Put your applicaiton javascript here
+(() => {
+    //Position absolute to sticky header on index
+    const nav = document.getElementById("home-nav");
+    document.addEventListener("scroll", () => {
+        //
+    });
+})();
+
+(() => {
+    //Full-Width Swiper
+    const mySwiper = new Swiper("#full-swiper-container", {
+        // Optional parameters
+        direction: "horizontal",
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: ".swiper-scrollbar",
+        },
+    });
+})();
