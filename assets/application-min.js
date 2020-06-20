0a23c1f7bd852d88d5fbb006570171383d23a38d
ben <gibumkang@gmail.com>
2020-06-19 18:39:07 -0700

    Add featured product

--- assets/application-min.js
+++
@@ -1,1 +0,0 @@
-(()=>{document.getElementById("home-nav");document.addEventListener("scroll",()=>{})})(),new Swiper("#full-swiper-container",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
--- assets/application.js
+++ assets/application.js
@@ -9,6 +9,31 @@
 (() => {
     //Full-Width Swiper
     const mySwiper = new Swiper("#full-swiper-container", {
+        effect: "fade",
+        // Optional parameters
+        direction: "horizontal",
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: ".swiper-scrollbar",
+        },
+    });
+
+    //Full-Width Swiper
+    const mySwiper2 = new Swiper("#featured-swiper-container", {
+        effect: "fade",
         // Optional parameters
         direction: "horizontal",
         loop: true,
